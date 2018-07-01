f88498e2f1a7d5a9c9d97257f758af2a0c421f8c
Adam Sawyer <aesawyer@live.com>
2018-07-01 00:54:42 -0600

    Python refactoring

--- Python/PasswordGenerator.py
+++ Python/PasswordGenerator.py
@@ -2,44 +2,35 @@ import random
 import os
 import sys
 
-def main():
+def Main():
     print('Password Generator - Python Implementation')
     print('--------------------------------------------')
-    print('How long would you like the password?')
-    lenCheck = passLength()
-    print('Upper-Case Letters? (Yes = 0, No = 1)')
-    upCheck = BoolCheck()
-    print('Numbers? (Yes = 0, No = 1)')
-    numCheck = BoolCheck()
-    print('Special Characters? (Yes = 0, No = 1)')
-    specCheck = BoolCheck()
-    print('Your randomly generated password is:')
-    finalPass = Generate(lenCheck, upCheck, numCheck, specCheck)
-    print('~~~~~~~~~~~~~~~~~~~~~~')
-    print(finalPass)
-    print('~~~~~~~~~~~~~~~~~~~~~~')
-    print('Generate another password? (Yes = 0, No = 1)')
-    genAgain = BoolCheck()
-    restartProg(genAgain)
+    lenCheck = PassLength('How long would you like the password: ')
+    upCheck = BoolCheck('Upper-Case Letters? (Yes = 0, No = 1): ')
+    numCheck = BoolCheck('Numbers? (Yes = 0, No = 1): ')
+    specCheck = BoolCheck('Special Characters? (Yes = 0, No = 1): ')
+    Generate(lenCheck, upCheck, numCheck, specCheck)
+    genAgain = BoolCheck('Generate another password? (Yes = 0, No = 1): ')
+    RestartProg(genAgain)
 
-def passLength():
+def PassLength(inputSentence):
     passlen = None
     while not passlen:
-        unchecked = input()
+        unchecked = input(inputSentence)
         try:
             passlen = int(unchecked)
             if passlen < 6:
                 passlen = None
-                print('    Invalid Entry. Password must be at least 6 characters in length')
+                print('    Password must be at least 6 characters in length')
             else:
                 return passlen
         except ValueError:
             print('    You must enter a number')
 
-def BoolCheck():
+def BoolCheck(inputSentence):
     upCheck = None
     while not upCheck:
-        boolCheck = input()
+        boolCheck = input(inputSentence)
         try:
             upCheck = int(boolCheck)
             if upCheck == 0 or upCheck == 1:
@@ -48,9 +39,9 @@ def BoolCheck():
                 upCheck = None
                 print('    You must enter 0 for Yes or 1 for No')
         except ValueError:
-            print('    You must enter True or False')
+            print('    You must enter 0 for Yes or 1 for No')
 
-def Generate(length, upCheck, numCheck, specCheck):
+def Generate(passlen, upCheck, numCheck, specCheck):
     alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     alphUp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     num = ['0','1','2','3','4','5','6','7','8','9']
@@ -62,16 +53,19 @@ def Generate(length, upCheck, numCheck, specCheck):
     if specCheck == 0:
         alph.extend(spec)
     charList = []
-    while len(charList) < length:
+    while len(charList) < passlen:
         charList.extend(random.choice(alph))
-    wordToPass = ''.join(charList)
-    return wordToPass
+    finalPass = ''.join(charList)
+    print('Your randomly generated password is:')
+    print('~~~~~~~~~~~~~~~~~~~~~~')
+    print(finalPass)
+    print('~~~~~~~~~~~~~~~~~~~~~~')
 
-def restartProg(genAgain):
+def RestartProg(genAgain):
     if genAgain == 0:
         os.system('cls')
-        main()
+        Main()
     else:
         sys.exit()
 
-main()
+Main()
